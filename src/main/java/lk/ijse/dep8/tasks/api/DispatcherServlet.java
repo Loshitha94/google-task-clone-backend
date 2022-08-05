807e9d2ca206ff53ecb25c603779841f5d1d7de0
loshitha <hasaralloshitha@gmail.com>
2022-08-05 17:16:51 +0530

    create user servlet class

--- src/main/java/lk/ijse/dep8/tasks/api/DispatcherServlet.java
+++ src/main/java/lk/ijse/dep8/tasks/api/DispatcherServlet.java
@@ -1,16 +1,23 @@
 package lk.ijse.dep8.tasks.api;
 
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import lk.ijse.dep8.tasks.security.SecurityContextHolder;
+import lk.ijse.dep8.tasks.util.HttpResponseErrorMsg;
 import lk.ijse.dep8.tasks.util.HttpServlet2;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@MultipartConfig(location = "/tmp", maxFileSize = 10*1024*1024)
 @WebServlet(name = "DispatcherServlet", value = "/v1/users/*")
 public class DispatcherServlet extends HttpServlet2 {
     @Override
@@ -24,6 +31,28 @@ public class DispatcherServlet extends HttpServlet2 {
 
             if (matcher.find()){
                 String userId= matcher.group(1);
+                if (!userId.equals(SecurityContextHolder.getPrincipal().getId())){
+                    resp.setContentType("application/json");
+                    resp.setStatus(403);
+                    Jsonb jsonb= JsonbBuilder.create();
+                    jsonb.toJson(new HttpResponseErrorMsg(new Date().getTime(),403,null,"Permission denied",
+                            req.getRequestURI()),resp.getWriter());
+                    return;
+                }
+            }
+
+            if(req.getPathInfo().matches("/[A-Fa-f0-9\\-]{36}/?")){
+                getServletContext().getNamedDispatcher("UserServlet").forward(req,resp);
+            } else if (req.getPathInfo().matches("/([A-Fa-f1-9\\-]{36})/lists(/\\d+)?/?")) {
+                    getServletContext().getNamedDispatcher("TaskListServlet").forward(req,resp);
+            }else if (req.getPathInfo().matches("/[A-Fa-f0-9\\-]{36}/lists/\\d+/tasks(/\\d+)?/?")) {
+                getServletContext().getNamedDispatcher("TaskServlet").forward(req, resp);
+            } else {
+                resp.setContentType("application/json");
+                resp.setStatus(404);
+                Jsonb jsonb = JsonbBuilder.create();
+                jsonb.toJson(new HttpResponseErrorMsg(new Date().getTime(), 404, null,
+                        "Invalid location", req.getRequestURI()), resp.getWriter());
             }
         }
     }
--- src/main/java/lk/ijse/dep8/tasks/api/UserServlet.java
+++ src/main/java/lk/ijse/dep8/tasks/api/UserServlet.java
@@ -1,7 +1,53 @@
 package lk.ijse.dep8.tasks.api;
 
+import lk.ijse.dep8.tasks.dto.UserDTO;
+import lk.ijse.dep8.tasks.util.HttpServlet2;
+import lk.ijse.dep8.tasks.util.ResponseStatusException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.logging.Logger;
 
-public class UserServlet extends HttpServlet {
+@WebServlet(name = "UserServlet")
+public class UserServlet extends HttpServlet2 {
 
+    private final Logger logger= Logger.getLogger(UserServlet.class.getName());
+
+    private UserDTO getUser(HttpServletRequest req){
+        if (!(req.getPathInfo()!=null &&
+                (req.getPathInfo().replaceAll("/","").length()==36))){
+            throw new ResponseStatusException(404,"Invalid user id");
+        }
+        String userId = req.getPathInfo().replaceAll("/","");
+
+        try {
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+    @Override
+    protected void doPatch(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPatch(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
+    }
 }
