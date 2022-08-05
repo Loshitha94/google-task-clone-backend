20f9f9995ffe45e6a1f45d98f933f775e458ab7c
loshitha <hasaralloshitha@gmail.com>
2022-08-05 11:24:20 +0530

    add security part to the application

--- src/main/java/lk/ijse/dep8/tasks/api/DispatcherServlet.java
+++ src/main/java/lk/ijse/dep8/tasks/api/DispatcherServlet.java
@@ -2,8 +2,29 @@ package lk.ijse.dep8.tasks.api;
 
 import lk.ijse.dep8.tasks.util.HttpServlet2;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+@WebServlet(name = "DispatcherServlet", value = "/v1/users/*")
 public class DispatcherServlet extends HttpServlet2 {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        if (req.getPathInfo()==null || req.getPathInfo().equals("/")){
+            getServletContext().getNamedDispatcher("UserServlet").forward(req,resp);
+        }else {
+            String pattern = "/([A-Fa-f0-9\\-]{36})/?.*";
+            Matcher matcher = Pattern.compile(pattern).matcher(req.getPathInfo());
+
+            if (matcher.find()){
+                String userId= matcher.group(1);
+            }
+        }
+    }
 }
