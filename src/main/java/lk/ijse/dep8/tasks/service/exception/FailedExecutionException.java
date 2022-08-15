f39d94559b31160e99bf885e82089c22a5d870f8
loshitha <hasaralloshitha@gmail.com>
2022-08-15 13:41:22 +0530

    create JNDI connection pool

---
+++ src/main/java/lk/ijse/dep8/tasks/service/exception/FailedExecutionException.java
@@ -0,0 +1,20 @@
+package lk.ijse.dep8.tasks.service.exception;
+
+public class FailedExecutionException extends RuntimeException{
+
+    public FailedExecutionException() {
+        super();
+    }
+
+    public FailedExecutionException(String message) {
+        super(message);
+    }
+
+    public FailedExecutionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public FailedExecutionException(Throwable cause) {
+        super(cause);
+    }
+}
