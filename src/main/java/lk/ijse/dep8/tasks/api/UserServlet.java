ce9db05f72c97aa0f36730db24f4d86be20a8500
loshitha <hasaralloshitha@gmail.com>
2022-08-15 15:56:22 +0530

    create methods for register users

--- src/main/java/lk/ijse/dep8/tasks/api/UserServlet.java
+++ src/main/java/lk/ijse/dep8/tasks/api/UserServlet.java
@@ -1,6 +1,8 @@
 package lk.ijse.dep8.tasks.api;
 
 import lk.ijse.dep8.tasks.dto.UserDTO;
+import lk.ijse.dep8.tasks.service.ServiceFactory;
+import lk.ijse.dep8.tasks.service.custom.UserService;
 import lk.ijse.dep8.tasks.util.HttpServlet2;
 import lk.ijse.dep8.tasks.util.ResponseStatusException;
 
@@ -18,18 +20,26 @@ public class UserServlet extends HttpServlet2 {
     private final Logger logger= Logger.getLogger(UserServlet.class.getName());
 
     private UserDTO getUser(HttpServletRequest req){
-        if (!(req.getPathInfo()!=null &&
-                (req.getPathInfo().replaceAll("/","").length()==36))){
-            throw new ResponseStatusException(404,"Invalid user id");
+        if (!(req.getPathInfo() != null &&
+                (req.getPathInfo().replaceAll("/", "").length() == 36))) {
+            throw new ResponseStatusException(404, "Invalid user id");
         }
-        String userId = req.getPathInfo().replaceAll("/","");
 
-        try {
+        String userId = req.getPathInfo().replaceAll("/", "");
 
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        try  {
+            UserService userService = ServiceFactory.getInstance().
+                    getService(ServiceFactory.ServiceTypes.USER);
+            if (!userService.existsUser(userId)) {
+                throw new ResponseStatusException(404, "Invalid user id");
+            } else {
+                return userService.getUser(userId);
+            }
+        } catch (ResponseStatusException e) {
+            throw e;
+        } catch (Throwable e) {
+            throw new ResponseStatusException(500, "Failed to fetch the user info", e);
         }
-        return null;
     }
     @Override
     protected void doPatch(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
