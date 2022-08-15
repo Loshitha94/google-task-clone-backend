8db2078245554dff2152389345dd3448ceac67c4
loshitha <hasaralloshitha@gmail.com>
2022-08-15 15:48:18 +0530

    create Service factory

--- src/main/java/lk/ijse/dep8/tasks/service/ServiceFactory.java
+++ src/main/java/lk/ijse/dep8/tasks/service/ServiceFactory.java
@@ -1,4 +1,31 @@
 package lk.ijse.dep8.tasks.service;
 
+import lk.ijse.dep8.tasks.service.custom.impl.TaskServiceImpl;
+import lk.ijse.dep8.tasks.service.custom.impl.UserServiceImpl;
+
 public class ServiceFactory {
+    private static ServiceFactory serviceFactory;
+
+    private ServiceFactory() {
+
+    }
+
+    public static ServiceFactory getInstance() {
+        return (serviceFactory == null) ? (serviceFactory = new ServiceFactory()) : serviceFactory;
+    }
+
+    public <T extends SuperService> T getService(ServiceTypes serviceType) {
+        switch (serviceType) {
+            case TASK:
+                return (T) new TaskServiceImpl();
+            case USER:
+                return (T) new UserServiceImpl();
+            default:
+                return null;
+        }
+    }
+
+    public enum ServiceTypes {
+        USER, TASK
+    }
 }
--- src/main/java/lk/ijse/dep8/tasks/service/custom/impl/UserServiceImpl.java
+++ src/main/java/lk/ijse/dep8/tasks/service/custom/impl/UserServiceImpl.java
@@ -1,9 +1,22 @@
 package lk.ijse.dep8.tasks.service.custom.impl;
 
+import lk.ijse.dep8.tasks.dao.DAOFactory;
+import lk.ijse.dep8.tasks.dao.SuperDAO;
+import lk.ijse.dep8.tasks.dao.custom.UserDAO;
 import lk.ijse.dep8.tasks.dto.UserDTO;
+import lk.ijse.dep8.tasks.entity.User;
 import lk.ijse.dep8.tasks.service.custom.UserService;
+import lk.ijse.dep8.tasks.service.exception.FailedExecutionException;
+import lk.ijse.dep8.tasks.service.util.EntityDTOMapper;
+import lk.ijse.dep8.tasks.service.util.JPAUtil;
+import org.apache.commons.codec.digest.DigestUtils;
 
+import javax.persistence.EntityManager;
 import javax.servlet.http.Part;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
 import java.util.logging.Logger;
 
 public class UserServiceImpl implements UserService {
@@ -12,12 +25,55 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public boolean existsUser(String userIdOrEmail) {
-        return false;
+        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+
+        try {
+            UserDAO userDAO = DAOFactory.getInstance().getDAO(em, DAOFactory.DAOTypes.USER);
+            return userDAO.existsUserByEmailOrId(userIdOrEmail);
+
+        }finally {
+            em.close();
+        }
     }
 
     @Override
     public UserDTO registerUser(Part picture, String appLocation, UserDTO user) {
-        return null;
+        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+        try {
+            em.getTransaction().begin();
+            user.setId(UUID.randomUUID().toString());
+
+            if (picture!=null){
+                user.setPicture(user.getPicture() + user.getId());
+            }
+            user.setPassword(DigestUtils.sha256Hex(user.getPassword()));
+
+            UserDAO dao = DAOFactory.getInstance().getDAO(em, DAOFactory.DAOTypes.USER);
+
+            User userEntity = EntityDTOMapper.getUser(user);
+            User savedUser = dao.save(userEntity);
+
+            user = EntityDTOMapper.getUserDTO(savedUser);
+            if (picture != null) {
+                Path path = Paths.get(appLocation, "uploads");
+                if (Files.notExists(path)) {
+                    Files.createDirectory(path);
+                }
+
+                String picturePath = path.resolve(user.getId()).toAbsolutePath().toString();
+                picture.write(picturePath);
+            }
+
+            em.getTransaction().commit();
+            return user;
+        } catch (Throwable t) {
+            if (em != null && em.getTransaction() != null) {
+                em.getTransaction().rollback();
+            }
+            throw new FailedExecutionException("Failed to save the user", t);
+        }finally {
+            em.close();
+        }
     }
 
     @Override
