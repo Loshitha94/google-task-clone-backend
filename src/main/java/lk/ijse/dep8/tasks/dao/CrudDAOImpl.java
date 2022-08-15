48182d9c2a78b8cab251a37c4317ae46f45d4a54
loshitha <hasaralloshitha@gmail.com>
2022-08-15 12:27:54 +0530

    create cruddaoimpl

---
+++ src/main/java/lk/ijse/dep8/tasks/dao/CrudDAOImpl.java
@@ -0,0 +1,51 @@
+package lk.ijse.dep8.tasks.dao;
+
+import lk.ijse.dep8.tasks.entity.SuperEntity;
+
+import javax.persistence.EntityManager;
+import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
+import java.util.Optional;
+
+public class CrudDAOImpl <T extends SuperEntity, ID extends Serializable> implements CrudDAO<T,ID>{
+
+    private final Class<T> entityClsObj;
+    protected EntityManager em;
+
+    public CrudDAOImpl() {
+        this.entityClsObj =  (Class<T>) (((ParameterizedType) (this.getClass().getGenericSuperclass())).getActualTypeArguments()[0]);
+    }
+
+
+    @Override
+    public boolean existById(ID pk) {
+        return findById(pk).isPresent();
+    }
+
+    @Override
+    public T save(T entity) {
+        return em.merge(entity);
+    }
+
+    @Override
+    public void deleteById(ID pk) {
+        em.remove(em.getReference(entityClsObj,pk));
+    }
+
+    @Override
+    public Optional<T> findById(ID pk) {
+        T entity = em.find(entityClsObj, pk);
+        return (entity == null) ? Optional.empty() : Optional.of(entity);
+    }
+
+    @Override
+    public List<T> findAll() {
+        return em.createQuery("SELECT e FROM "+entityClsObj.getName()+" e",entityClsObj).getResultList();
+    }
+
+    @Override
+    public long count() {
+        return em.createQuery("SELECT COUNT(entity) FROM "+ entityClsObj.getName() +" entity", Long.class).getSingleResult();
+    }
+}
